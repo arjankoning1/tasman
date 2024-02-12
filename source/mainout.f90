7c8a00588710da050c178ccae457cd0cd07c4524
Arjan Koning <arjankoning1@gmail.com>
2024-02-12 17:39:09 +0100

    New version

--- source/mainout.f90
+++ source/mainout.f90
@@ -8,11 +8,17 @@ subroutine mainout
 ! 2023-12-29: Original code
 !-----------------------------------------------------------------------------------------------------------------------------------
 !
-! *** Declaration of local data
+! *** Use data from other modules
+!
+  use A0_tasman_mod
+!
+! Main output
 !
   write(*,'(/"    TASMAN-2.0 (Version: December 29, 2023)"/)')
   write(*, '(" Copyright (C) 2023  A.J. Koning"/)')
   write(*, '(" Dimensions - Cross sections: mb, Energies: MeV, ", "Angles: degrees")')
+  write(*, '(/" User: ",a)') trim(user)
+  write(*, '(" Date: ",a)') trim(date)
 !
 ! ***************** Write input file and default parameters ************
 !
