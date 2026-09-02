93add5347c3e5754c34dbc16bf4f420f97dd0196
Arjan Koning <arjankoning1@gmail.com>
2026-09-02 23:15:00 +0200

    update

---
+++ source/deallocate_arrays.f90
@@ -0,0 +1,23 @@
+subroutine deallocate_arrays
+!
+!-----------------------------------------------------------------------------------------------------------------------------------
+! Purpose   : Deallocate dynamic arrays
+!-----------------------------------------------------------------------------------------------------------------------------------
+!
+  use A0_tasman_mod
+  implicit none
+!
+  if (allocated(xssave)) deallocate(xssave)
+  if (allocated(rpsave)) deallocate(rpsave)
+  if (allocated(prodsave)) deallocate(prodsave)
+  if (allocated(gamsave)) deallocate(gamsave)
+  if (allocated(S)) deallocate(S)
+  if (allocated(Sdenom)) deallocate(Sdenom)
+  if (allocated(Senum)) deallocate(Senum)
+  if (allocated(Pearson)) deallocate(Pearson)
+  if (allocated(Pearson_enum)) deallocate(Pearson_enum)
+  if (allocated(Pearson_denom_par)) deallocate(Pearson_denom_par)
+  if (allocated(Pearson_denom_xs)) deallocate(Pearson_denom_xs)
+!
+  return
+end subroutine deallocate_arrays
