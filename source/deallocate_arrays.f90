3e7d9dbc6e5de97a7f0dc0afb40aff5d6e2eebeb
Arjan Koning <arjankoning1@gmail.com>
2026-09-06 16:47:26 +0200

    update

--- source/deallocate_arrays.f90
+++ source/deallocate_arrays.f90
@@ -42,6 +42,7 @@ subroutine deallocate_arrays
   if (allocated(Pearson_denom_par)) deallocate(Pearson_denom_par)
   if (allocated(Pearson_denom_xs)) deallocate(Pearson_denom_xs)
   if (allocated(Gchannelsave)) deallocate(Gchannelsave)
+  if (allocated(xseffsave)) deallocate(xseffsave)
 !
   return
 end subroutine deallocate_arrays
