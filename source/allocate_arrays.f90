b8818f8b01c667fd5ba84c8a1bef4357d223790d
Arjan Koning <arjankoning1@gmail.com>
2026-09-06 16:15:33 +0200

    update

--- source/allocate_arrays.f90
+++ source/allocate_arrays.f90
@@ -10,21 +10,74 @@ subroutine allocate_arrays
   if (flagcross) then
     allocate(xssave(0:Ntalys,numchanxs,0:numenS))
     xssave = 0.
+    allocate(xsav(numchanxs,numenin))
+    xsav = 0.
+    allocate(xsavC(numchanxs,numencov))
+    xsavC = 0.
+    allocate(Vmt(numchanxs,numencov,numencov))
+    Vmt = 0.
+    allocate(Rmt(numchanxs,numencov,numencov))
+    Rmt = 0.
+    allocate(RmtD(numchanxs,numenin))
+    RmtD = 0.
+    allocate(Rcov(numchancov,numencov,numchancov,numencov))
+    Rcov =0.
+    allocate(Vcov(numchancov,numencov,numchancov,numencov))
+    Vcov = 0.
   endif
 
   if (flagresidual) then
     allocate(rpsave(0:Ntalys,numchanrp,0:numenS))
     rpsave = 0.
+    allocate(rpav(numchanrp,numenin))
+    rpav = 0.
+    allocate(Rrp(numchanrp,numenin,numencov))
+    Rrp = 0.
+    allocate(RrpD(numchanrp,numenin))
+    RRpD = 0.
+  endif
+
+  if (flagspectra) then
+    allocate(Rsp(numchansp,0:numen2, 0:numen2))
+    Rsp = 0.
+    allocate(spav(numchansp,0:numen2))
+    spav = 0.
   endif
 
   if (flagprod) then
     allocate(prodsave(0:Ntalys,numchanprod,0:numenS))
     prodsave = 0.
+    allocate(prodav(numchanprod,numenin))
+    prodav = 0.
+    allocate(Rprod(numchanprod,numencov,numencov))
+    Rprod = 0.
+    allocate(RprodD(numchanprod,numenin))
+    RprodD = 0.
+  endif
+
+  if (flagangle) then
+    allocate(angav(numchanang,0:numang))
+    angav = 0.
+    allocate(Rang(numchanang,0:numang,0:numang))
+    Rang = 0.
+  endif
+
+  if (flagleg) then
+    allocate(legav(numchanang,0:numleg))                 
+    legav = 0.
+    allocate(Rleg(numchanang,0:numleg,numchanang,0:numleg))
+    Rleg = 0.
   endif
 
   if (flaggamma) then
     allocate(gamsave(0:Ntalys,numchangam,0:numenS))
     gamsave = 0.
+    allocate(gamav(numchangam,numenin))
+    gamav = 0.
+    allocate(Rgam(numchangam,numencov,numencov))
+    Rgam = 0.
+    allocate(RgamD(numchangam,numenin))
+    RgamD = 0.
   endif
 ! allocate(parsave(0:Ntalys,Npar))
 ! parsave = 0.
